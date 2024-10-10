e5e27b3bca7137fbf4d51a3dc68bbf721863ad9e
Simão Ildefonso <simaoildefonso@ua.pt>
2024-10-10 16:19:54 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -10,42 +10,46 @@ import CarDetails from './components/CarDetails';
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Car Dealership</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/cars">Cars</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">About Us</Link>
-              </li>
-            </ul>
+      <div className="d-flex flex-column min-vh-100">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Car Dealership</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cars">Cars</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About Us</Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cars" element={<CarsList />} />
-          <Route path="/car/:id" element={<CarDetails />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <main className="flex-grow-1">
+          <div className="container mt-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cars" element={<CarsList />} />
+              <Route path="/car/:id" element={<CarDetails />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </main>
+
+        <Footer
+          company="Car Dealership"
+          year={new Date().getFullYear()}
+        />
       </div>
-      <Footer
-        company="Car Dealership"
-        year={new Date().getFullYear()}
-      />
     </Router>
-    
   );
 }
 
